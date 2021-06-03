411e0b89dee2d59e4ab730707f7645a905d55aa3
Ramiro Miguez <ramiromiguezlauria@gmail.com>
2021-06-03 20:24:02 -0300

    EasySkins-ItemListContainer

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = props =>  {
+    return (
+        <div>
+            <h1>{props.greeting}</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer;
