d2eb97a4d55e071d1a98bc9c9985d0f3bcf10462
Ramiro Miguez <ramiromiguezlauria@gmail.com>
2021-07-08 07:30:00 -0300

    correction1.0

--- src/components/itemdetailcontainer/itemCount/ItemCount.jsx
+++ src/components/itemdetailcontainer/itemCount/ItemCount.jsx
@@ -8,30 +8,9 @@ const ShowMessage = ({stock}) =>{
      return <h4>{`Vas a comprar ${stock}`}</h4>
  }
 
- const ButtonPlusOne = ({stock, changeStock, maxStock, StockLeft, id, stockQuantity}) =>{
-    const [stockLeft, setStockLeft] = useState();
-    const [stockTotal, setStockTotal] = useState();
-
-    useEffect(() => {
-        setTimeout(() =>{
-        setStockLeft(StockLeft(id))
-        setStockTotal(StockLimit)
-        console.log(stockLeft)
-        console.log(stockTotal)
-        }, 2000)
-    }, [id])
-
-    const StockLimit = () =>{
-        console.log(`stockLeft ${stockLeft}`)
-        if(stockLeft > 0){
-            return stockTotal
-        }
-        else{
-            return 0
-        }
-    }
-    
-     return <button className="boton" onClick={() => changeStock(stock + 1) + console.log(stockLeft) + console.log(`xd${stockQuantity}`)} disabled={stock<maxStock?false:true}> +1 </button>
+ const ButtonPlusOne = ({stock, changeStock, maxStock, StockLeft, id}) =>{
+    const [stockQuantity, setStockQuantity] = useState();
+     return <button className="boton" onClick={() => changeStock(stock + 1)} disabled={stock<maxStock?false:true}> +1 </button>
 }
 
 const ButtonMinusOne = ({stock, changeStock}) =>{
@@ -50,32 +29,27 @@ export const ItemCount = props =>{
     const handlePress = value => setPress(value);
     const stockMaxQuantity = props.element.stock
     const stockId = props.element.id
-    const [addItems, setAddItems, AddNewItem, RemoveItem, IsInCar, ClearCart, StockLeft] = useContext(CartContext)
+    const [addItems, setAddItems, AddNewItem, RemoveItem, IsInCar, ClearCart, StockLeft, addStock, setAddStock] = useContext(CartContext)
     const itemsDataArray = props.elementData
     const [stockQuantity, setStockQuantity] = useState();
-
-    useEffect(() => {
-        //setTimeout(() =>{
-        setStockQuantity(StockLeft(stockId))
-        //}, 2000)
-    }, [stockId])
     
     return (press===false) ?
         <div className="butonContainer">
             <ShowMessage stock={stock}/>
             <ButtonMinusOne stock={stock} changeStock={handleStock}/>
-            <ButtonAddCart stock={stock} press={press} changePress={handlePress} addItem={AddNewItem} id={stockId} maxStock={stockMaxQuantity} stockQuantity={stockQuantity}/>
-            <ButtonPlusOne  stock={stock} changeStock={handleStock} maxStock={stockMaxQuantity} StockLeft={StockLeft} id={stockId}/>
+            <ButtonAddCart stock={stock} changePress={handlePress}/>
+            <ButtonPlusOne  stock={stock} changeStock={handleStock} addStock={addStock} id={stockId}/>
             <button className="boton" onClick={() => IsInCar(stockId)}> Comprobar existencia</button>
         </div>
         :
         <div className="butonContainer">
-            <Link className="boton" to={`/Cart`} onClick={() => AddNewItem(stockId,stock, stockMaxQuantity, itemsDataArray)}>Confirmar Compra</Link>
+            <Link className="boton" to={`/Cart`} onClick={() => AddNewItem(stockId,stock, stockMaxQuantity, itemsDataArray) + StockLeft(stockId, stock)}>Confirmar Compra</Link>
             <button className="boton" onClick={() => handlePress(false)}> Deshacer compra</button>
             <button className="boton" onClick={() => console.log(addItems)}> Comprobar contexto</button>
             <button className="boton" onClick={() => RemoveItem(stockId)}> Eliminar este item</button>
             <button className="boton" onClick={() => IsInCar(stockId)}> Comprobar existencia</button>
             <button className="boton" onClick={() => ClearCart()}> Clear context</button>
+            <button className="boton" onClick={() => console.log(addStock)}> stockLeft</button>
 
         </div>       
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,26 +5,21 @@ export const CartContext = React.createContext([])
 
 export const CartContextProvider = ({children}) => {
     const [addItems, setAddItems] = useState([])
+    const [addStock, setAddStock] = useState([])
 
     const AddNewItem = (stockDataId, stockSelected, maxStock, itemsDataArray) =>{
-        const findId = addItems.find(element => element.id === stockDataId);//find me filtra el addItem por id 
+        const findId = addItems.find(element => element.id === stockDataId); 
         const findDataId = itemsDataArray.find(element => element.id === stockDataId);
-        if(findId && findDataId){//si find existe significa que ya se habia cargado un item con este id
-            if(stockSelected<=maxStock){// esto es provicional xd
-                const new_items = (addItems.filter(element => element.id !== stockDataId)) // filtro todos los items menos el seleccionado
-                const object = {// le cargo la data
-                    id : stockDataId,
-                    stock: stockSelected + findId.stock,
-                    price: findDataId.price,
-                    name: findDataId.gunName,
-                    stockLimited: findId.stockLimit
-                }
-            setAddItems([...new_items, object])// piso el nuevo item a los viejos
+        if(findId){
+            const new_items = (addItems.filter(element => element.id !== stockDataId)) 
+            const object = {
+                id : stockDataId,
+                stock: stockSelected + findId.stock,
+                price: findDataId.price,
+                name: findDataId.gunName,
+                stockLimited: findId.stockLimit
             }
-            else{
-                return null
-            }
-            return null
+            return setAddItems([...new_items, object])
         }
         const object = {
             id : stockDataId,
@@ -33,7 +28,25 @@ export const CartContextProvider = ({children}) => {
             name: findDataId.gunName,
             stockLimit: maxStock
         }
-        setAddItems([...addItems, object])
+        return setAddItems([...addItems, object])
+    }
+
+    const  StockLeft = (id, stockSelected) => {
+        const findItem = addStock.find(element => element.id === id)
+        if(findItem){
+            const new_items = (addStock.filter(element => element.id !== id)) 
+            const object = {
+                id: id,
+                stock: stockSelected + findItem.stock
+            }
+        return setAddStock([...new_items, object])
+        }
+        const object = {
+            id : id,
+            stock: stockSelected
+        }
+        setAddStock([...addStock, object])
+        return 0
     }
 
     const RemoveItem = (id) => {
@@ -56,17 +69,8 @@ export const CartContextProvider = ({children}) => {
         setAddItems([])
     } 
 
-    const  StockLeft = (id) => {
-        const findItem = addItems.find(element => element.id === id)
-        if(findItem){
-           return findItem.stock
-        }
-        console.log("No Existe")
-    }
-
-
     return (
-        <CartContext.Provider value={[addItems, setAddItems, AddNewItem, RemoveItem, IsInCar, ClearCart, StockLeft]}>
+        <CartContext.Provider value={[addItems, setAddItems, AddNewItem, RemoveItem, IsInCar, ClearCart, StockLeft, addStock, setAddStock]}>
             {children}
         </CartContext.Provider>
     )
