ba47bcdf337a405784874760d9a33d5f9585adaa
Ramiro <ramiromiguezlauria@gmail.com>
2021-07-15 17:24:29 -0300

    firebase 0

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,7 +2,13 @@ import firebase from "firebase/app";
 import 'firebase/firestore'
 
 // web apps firebase config
-var firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyAYWTx0da1fWpPa8dY0qSI8KIeZohYBaCM",
+  authDomain: "easyskins-app.firebaseapp.com",
+  projectId: "easyskins-app",
+  storageBucket: "easyskins-app.appspot.com",
+  messagingSenderId: "38083420533",
+  appId: "1:38083420533:web:0196142b38652ad8e0733c"
 };
 
 // initialize firebase
